797e4681c53e147c52aeff0024314121fc625a6a
Manuel Zamora <mazamora808@gmail.com>
2022-04-02 12:31:11 -0500

    Add CameraController

---
+++ Assets/Scripts/Visuals/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare50.Client.Visuals
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+
+        private Camera _mainCamera;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _mainCamera = GetComponent<Camera>();
+            CalculateCamSize();
+        }
+
+        private void CalculateCamSize()
+        {
+            _mainCamera.orthographicSize = Mathf.Min(_spriteRenderer.bounds.extents.y, _spriteRenderer.bounds.extents.x / _mainCamera.aspect);
+        }
+    }
+}

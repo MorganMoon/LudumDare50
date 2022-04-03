af48d3273b3e820bd24cc34013dd9c31f90c36a9
Morgan Moon <morgan@cerberusinteractive.com>
2022-04-02 19:58:18 -0500

    Organized the scene to complete the game loop

--- Assets/Scripts/States/Gameplay/GameplayState.cs
+++ Assets/Scripts/States/Gameplay/GameplayState.cs
@@ -3,6 +3,7 @@ using LudumDare50.Client.Data;
 using LudumDare50.Client.Game;
 using LudumDare50.Client.Infrastructure;
 using LudumDare50.Client.ViewModels.Energy;
+using UnityEngine;
 using Zenject;
 
 namespace LudumDare50.Client.States.Gameplay
@@ -24,19 +25,23 @@ namespace LudumDare50.Client.States.Gameplay
         private readonly ISleepService _sleepService;
         private readonly IStateController<GameplayStateEvent> _gameplayStateController;
         private readonly IGameTime _gameTime;
+        private readonly Transform _gameplayArea;
 
         [Inject]
         public GameplayState(IScreenService screenService, ISleepService sleepService,
-            IStateController<GameplayStateEvent> gameplayStateController, IGameTime gameTime)
+            IStateController<GameplayStateEvent> gameplayStateController, IGameTime gameTime,
+            [Inject(Id = "GameplayArea")]Transform gameplayArea)
         {
             _screenService = screenService;
             _sleepService = sleepService;
             _gameplayStateController = gameplayStateController;
             _gameTime = gameTime;
+            _gameplayArea = gameplayArea;
         }
 
         public override void OnEnter()
         {
+            _gameplayArea.gameObject.SetActive(true);
             _sleepService.Start();
             _gameTime.Start();
             _screenService.SetActiveScreen<EnergyViewModel, Energy>(_sleepService.Energy);
@@ -46,6 +51,7 @@ namespace LudumDare50.Client.States.Gameplay
         {
             _sleepService.Stop();
             _gameTime.Stop();
+            _gameplayArea.gameObject.SetActive(false);
         }
 
         public void Tick()

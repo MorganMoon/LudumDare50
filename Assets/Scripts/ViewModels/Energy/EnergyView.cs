39fd7f5b6e4c8f7c46472a20f1d2b0325379a4ec
JWSmith2 <walker.smith.1997@gmail.com>
2022-04-04 00:20:06 -0500

    Updated Energy View Model to use slider

--- Assets/Scripts/ViewModels/Energy/EnergyView.cs
+++ Assets/Scripts/ViewModels/Energy/EnergyView.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LudumDare50.Client.ViewModels.Energy
 {
@@ -7,6 +8,12 @@ namespace LudumDare50.Client.ViewModels.Energy
     {
         [SerializeField]
         private TMP_Text _energyText;
+
+        [SerializeField]
+        private Slider _energySlider;
+
+        [SerializeField]
+        private Image _fillImage;
 
         protected override void SetBindings()
         {
@@ -20,6 +27,19 @@ namespace LudumDare50.Client.ViewModels.Energy
             {
                 var splitText = _energyText.text.Split('/');
                 _energyText.text = $"{currentEnergy.ToString("N1")}/{splitText[1]}";
+
+                var percent = currentEnergy / float.Parse(splitText[1]);
+                _energySlider.value = percent;
+
+                if(percent > 0.5)
+                {
+                    _fillImage.color = Color.Lerp(Color.green, Color.yellow, 1 - ((percent - 0.5f) * 2));
+                }
+                else
+                {
+                    _fillImage.color = Color.Lerp(Color.yellow, Color.red, 1 - (percent * 2));
+
+                }
             }, nameof(ViewModel.CurrentEnergy));
         }
     }
